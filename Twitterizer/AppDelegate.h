d49308857fc943031382c9c8f9ede8c4217e9dad
Jouba Ayedi <joubausa@icloud.com>
2016-05-25 14:17:16 -0500

    Initial Commit

---
+++ Twitterizer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitterizer
+//
+//  Created by Mohamed Ayadi on 5/25/16.
+//  Copyright © 2016 Mohamed Ayadi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Twitterizer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Twitterizer
+//
+//  Created by Mohamed Ayadi on 5/25/16.
+//  Copyright © 2016 Mohamed Ayadi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
